fe17312abfb7a931105bba1fe801cdc2a798b509
gmkleinman <gmkleinman@gmail.com>
2020-05-18 15:36:56 -0700

    Add initial skeleton.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const canvasEl = document.getElementById("canvas");
+    const ctx = canvas.getContext('2d');
+    
+    // ctx.fillStyle='D0ECE7'
+    // ctx.fillRect='0,0,1000,800'
+    canvasEl.width = 800;
+    canvasEl.height = 600;
+
+
+
+})
