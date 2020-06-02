cb780d1a291d29154a9fcd56b25839be3892f144
gmkleinman <gmkleinman@gmail.com>
2020-06-01 21:27:14 -0700

    Add initial blob class

---
+++ src/blob.js
@@ -0,0 +1,10 @@
+class Blob extends Unit{
+    constructor() {
+        super({
+            pos: obj.pos,
+            vel: obj.vel,
+            team: obj.team,
+            type: obj.type,
+        })
+    }
+}
